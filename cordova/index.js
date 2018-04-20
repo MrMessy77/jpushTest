f833b32a00f5c00bc91cb4f1bb9bf04da23eaccf
MrMessy77 <313691730@qq.com>
2018-04-20 15:11:40 +0800

    init

---
+++ cordova/index.js
@@ -0,0 +1,29 @@
+const loadJs = (url,cb)=>{
+    var script = document.createElement('script');
+    script.setAttribute('src', url);
+    script.setAttribute('type', 'text/javascript');
+    var loaded = false;
+    var loadFunction = function () {
+        if (loaded) return;
+        loaded = true;
+        cb && cb();
+    };
+    script.onload = loadFunction;
+    script.onreadystatechange = loadFunction;
+    document.getElementsByTagName("head")[0].appendChild(script);
+};
+const loadCordova = ()=>{
+    if(/RunningWithCordova/i.test(navigator.userAgent)){
+        let cordova_name = "cordova.js";
+        if(/android/i.test(navigator.userAgent)){
+            cordova_name = 'android/cordova.js';
+        }
+        else if(/iPhone/i.test(navigator.userAgent)){
+            cordova_name = 'ios/cordova.js';
+        }
+
+        loadJs(cordova_name,function(){});
+    };
+};
+
+loadCordova();
